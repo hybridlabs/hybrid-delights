eeed6c73d3b279898c139066da1d26fa392d1ab2
MysticKoko <132279944+MysticKoko@users.noreply.github.com>
2026-08-07 00:35:42 +0300

    Help por favor

--- fabric/src/main/kotlin/dev/hybridlabs/delights/data/client/ModelProvider.kt
+++ fabric/src/main/kotlin/dev/hybridlabs/delights/data/client/ModelProvider.kt
@@ -12,9 +12,6 @@ class ModelProvider(output: FabricDataOutput) : FabricModelProvider(output) {
 
     override fun generateItemModels(generator: ItemModelGenerators) {
         setOf(
-            HDItems.CORAL_KNIFE.get(),
-            HDItems.SHARK_TOOTH_KNIFE.get(),
-
             HDItems.BRINE_BOTTLE.get(),
             HDItems.SALT.get(),
             HDItems.PICKLED_HERRING.get(),
@@ -73,6 +70,12 @@ class ModelProvider(output: FabricDataOutput) : FabricModelProvider(output) {
             HDItems.TUNA_SANDWICH.get(),
         ).forEach { item ->
             generator.generateFlatItem(item, ModelTemplates.FLAT_ITEM)
+        }
+        setOf(
+            HDItems.CORAL_KNIFE.get(),
+            HDItems.SHARK_TOOTH_KNIFE.get(),
+        ).forEach { item ->
+            generator.generateFlatItem(item, ModelTemplates.FLAT_HANDHELD_ITEM)
         }
     }
 }
