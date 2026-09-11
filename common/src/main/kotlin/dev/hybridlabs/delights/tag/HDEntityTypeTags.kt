db91dd4995c96144e757dea79610b3bc0daaf346
MysticKoko <132279944+MysticKoko@users.noreply.github.com>
2026-09-12 01:31:28 +0300

    @dan I lied this is harder than it looked, pls complete it

---
+++ common/src/main/kotlin/dev/hybridlabs/delights/tag/HDEntityTypeTags.kt
@@ -0,0 +1,17 @@
+package dev.hybridlabs.delights.tag
+
+import dev.hybridlabs.delights.compat.ModIds
+import net.minecraft.core.registries.Registries
+import net.minecraft.resources.ResourceLocation
+import net.minecraft.tags.TagKey
+import net.minecraft.world.entity.EntityType
+
+object HDEntityTypeTags {
+    //#region Hybrid Aquatic Tags
+    val ALL_JELLYFISH = createHybridAquaticTag("all_jellyfish")
+    //#endregion
+
+    private fun createHybridAquaticTag(id: String): TagKey<EntityType<*>> {
+        return TagKey.create(Registries.ENTITY_TYPE, ResourceLocation(ModIds.HYBRID_AQUATIC, id))
+    }
+}
